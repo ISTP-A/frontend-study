5c65946675a97a47d206de160a94a1fe16ce5cc0
KIM SANG HUN <huhuhunuhuhn@gmail.com>
2025-09-16 14:07:24 +0900

    chore: add and configure tailwindcss

--- src/pages/count/count-page.tsx
+++ src/pages/count/count-page.tsx
@@ -4,8 +4,13 @@ export default function CountPage() {
     const [count, setCount] = useState<number>(0)
     return (
         <div>
-            {count}
-            <button type='button' onClick={() => setCount(prev => prev + 1)}>버튼</button>
+            <p>{count}</p>
+            <button
+                type='button'
+                onClick={() => setCount(prev => prev + 1)}
+            >
+                버튼
+            </button>
         </div>
     )
 }
